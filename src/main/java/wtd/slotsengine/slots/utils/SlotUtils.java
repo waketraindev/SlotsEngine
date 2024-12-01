7b44dae52699be7db22145901dc71db7efe78d4d
WakeTrainDev <175499942+waketraindev@users.noreply.github.com>
2024-12-01 17:54:59 +0200

    Add static util functions to encode/decode data strings

--- src/main/java/wtd/slotsengine/slots/utils/SlotUtils.java
+++ src/main/java/wtd/slotsengine/slots/utils/SlotUtils.java
@@ -1,4 +1,31 @@
 package wtd.slotsengine.slots.utils;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Base64;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
+
 public class SlotUtils {
+    public static String encodeGzipBase64(byte[] data) {
+        try {
+            ByteArrayOutputStream output = new ByteArrayOutputStream();
+            GZIPOutputStream comp = new GZIPOutputStream(output);
+            comp.write(data, 0, data.length);
+            comp.close();
+            output.close();
+            return Base64.getEncoder().encodeToString(output.toByteArray());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static byte[] decodeGzipBase64(String dataString) {
+        try (GZIPInputStream comp = new GZIPInputStream(new ByteArrayInputStream(Base64.getDecoder().decode(dataString)))) {
+            return comp.readAllBytes();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
