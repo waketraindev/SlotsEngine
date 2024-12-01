65d082e46113c17d53bf1971a80ffe00be87ddfc
WakeTrainDev <175499942+waketraindev@users.noreply.github.com>
2024-12-01 22:35:10 +0200

    save game

--- src/main/java/wtd/slotsengine/rest/controllers/ApiController.java
+++ src/main/java/wtd/slotsengine/rest/controllers/ApiController.java
@@ -3,11 +3,18 @@ package wtd.slotsengine.rest.controllers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+import wtd.slotsengine.rest.exceptions.InvalidSubscriberException;
 import wtd.slotsengine.rest.records.ServerVersionMessage;
 import wtd.slotsengine.services.LiveEventsManager;
+import wtd.slotsengine.services.LiveSubscriber;
 import wtd.slotsengine.utils.SlotUtils;
+
+import java.util.UUID;
 
 @RestController
 public class ApiController implements InitializingBean {
@@ -20,14 +27,22 @@ public class ApiController implements InitializingBean {
         this.live = liveEventsManager;
     }
 
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        log.info("API controller is initialized");
+    }
 
     @GetMapping("/api")
     public ServerVersionMessage indexAction() {
         return SERVER_BANNER;
     }
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        log.info("API controller is initialized");
+    @GetMapping("/api/{uid}")
+    public LiveSubscriber eventsAction(@PathVariable("uid") String userid) {
+        try {
+            return live.getSubscriberByUID(UUID.fromString(userid));
+        } catch (InvalidSubscriberException e) {
+            throw new ResponseStatusException(HttpStatusCode.valueOf(404));
+        }
     }
 }
