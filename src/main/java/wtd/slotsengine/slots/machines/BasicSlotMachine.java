f10fff679677726e9a8cfc85b02b5928bd337b4c
WakeTrainDev <175499942+waketraindev@users.noreply.github.com>
2024-12-01 15:43:11 +0200

    Checkpoint slot machine

--- src/main/java/wtd/slotsengine/slots/machines/BasicSlotMachine.java
+++ src/main/java/wtd/slotsengine/slots/machines/BasicSlotMachine.java
@@ -1,17 +1,21 @@
 package wtd.slotsengine.slots.machines;
 
 import wtd.slotsengine.slots.exceptions.InsufficientFundsException;
+import wtd.slotsengine.slots.exceptions.SlotUserException;
 import wtd.slotsengine.slots.interfaces.SlotMachine;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.LongSummaryStatistics;
 import java.util.Random;
 import java.util.concurrent.atomic.AtomicLong;
 
 final public class BasicSlotMachine implements SlotMachine {
     private final AtomicLong credits = new AtomicLong(0);
     private final Random rng;
-    private final List<Integer> reel = new ArrayList<Integer>();
+    private final List<Integer> reel = new ArrayList<>();
+    private final LongSummaryStatistics winStats = new LongSummaryStatistics();
+    private final LongSummaryStatistics betStats = new LongSummaryStatistics();
 
     public BasicSlotMachine() {
         this.rng = new Random();
@@ -24,6 +28,14 @@ final public class BasicSlotMachine implements SlotMachine {
         }
     }
 
+    public LongSummaryStatistics getWinStats() {
+        return winStats;
+    }
+
+    public LongSummaryStatistics getBetStats() {
+        return betStats;
+    }
+
     @Override
     public long spin(long betAmount) {
         assertFunds(betAmount, "spin");
@@ -33,34 +45,9 @@ final public class BasicSlotMachine implements SlotMachine {
         long winAmount = spinLogic(position, betAmount);
         if (winAmount > 0) {
             credits.addAndGet(winAmount);
+            winStats.accept(winAmount);
         }
-        return winAmount;
-    }
-
-    public double testLogic() {
-        long cost = 0L;
-        long winAmount = 0L;
-
-        for (int i = 0; i < reel.size(); i++) {
-            winAmount += spinLogic(i, 1);
-            cost += 1;
-        }
-
-        return winAmount / (double) cost;
-    }
-
-    private long spinLogic(int position, long betAmount) {
-        long winAmount = 0L;
-        int res = reel.get(position);
-
-        switch (res) {
-            case 0 -> winAmount = 0;
-            case 1 -> winAmount = betAmount * 1;
-            case 2 -> winAmount = betAmount * 2;
-            case 3 -> winAmount = betAmount * 3;
-            default -> winAmount = 0;
-        }
-
+        betStats.accept(betAmount);
         return winAmount;
     }
 
@@ -75,13 +62,38 @@ final public class BasicSlotMachine implements SlotMachine {
         return credits.addAndGet(-withdrawAmount);
     }
 
+    @Override
+    public long getBalance() {
+        return credits.get();
+    }
+
     private void assertFunds(long requiredAmount, String actionName) {
         if (requiredAmount > credits.get())
             throw new InsufficientFundsException("Insufficient credits to %s. Required: %d Have: %d".formatted(actionName, requiredAmount, credits.get()));
     }
 
-    @Override
-    public long getBalance() {
-        return credits.get();
+    private long spinLogic(int position, long betAmount) {
+        long winAmount;
+        int res = reel.get(position);
+
+        switch (res) {
+            case 0 -> winAmount = 0;
+            case 1 -> winAmount = betAmount;
+            case 2 -> winAmount = betAmount * 2;
+            case 3 -> winAmount = betAmount * 3;
+            default -> throw new SlotUserException("Invalid symbol");
+        }
+
+        return winAmount;
+    }
+
+    public double calculateRTP() {
+        long cost = 0L;
+        long winAmount = 0L;
+        for (int i = 0; i < reel.size(); i++) {
+            winAmount += spinLogic(i, 1);
+            cost += 1;
+        }
+        return winAmount / (double) cost;
     }
 }
