710813dcaccaebc91bd8f10b90f66872d2a70051
WakeTrainDev <175499942+waketraindev@users.noreply.github.com>
2024-12-01 20:24:51 +0200

    Checkpoint work

--- src/main/java/wtd/slotsengine/rest/ApiController.java
+++ src/main/java/wtd/slotsengine/rest/ApiController.java
@@ -1,5 +1,8 @@
 package wtd.slotsengine.rest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import wtd.slotsengine.rest.records.ServerBannerMessage;
@@ -7,16 +10,24 @@ import wtd.slotsengine.services.LiveEventsManager;
 import wtd.slotsengine.utils.SlotUtils;
 
 @RestController
-public class ApiController {
+public class ApiController implements InitializingBean {
     public static final ServerBannerMessage SERVER_BANNER = new ServerBannerMessage(SlotUtils.PROJECT_VERSION);
+    private static final Logger log = LoggerFactory.getLogger(ApiController.class);
     private final LiveEventsManager live;
 
     public ApiController(LiveEventsManager liveEventsManager) {
+        log.info("API controller is initializing");
         this.live = liveEventsManager;
     }
+
 
     @GetMapping("/api")
     public ServerBannerMessage indexAction() {
         return SERVER_BANNER;
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        log.info("API controller is initialized");
     }
 }
--- src/main/java/wtd/slotsengine/services/LiveEventsManager.java
+++ src/main/java/wtd/slotsengine/services/LiveEventsManager.java
@@ -2,13 +2,32 @@ package wtd.slotsengine.services;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+
 @Service
-public class LiveEventsManager {
+public class LiveEventsManager implements InitializingBean, DisposableBean {
     private static final Logger log = LoggerFactory.getLogger(LiveEventsManager.class);
+    private final ScheduledExecutorService scheduler;
 
     public LiveEventsManager() {
         log.info("Live events manager is initializing.");
+        this.scheduler = Executors.newSingleThreadScheduledExecutor();
+
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        log.info("Live events manager is shutting down.");
+        scheduler.shutdown();
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        log.info("Live events manager is initialized.");
     }
 }
