aa7a6c5126edb4406731d57fd3b430887bc63301
WakeTrainDev <175499942+waketraindev@users.noreply.github.com>
2024-12-01 10:27:12 +0200

    Project Creation

---
+++ src/main/java/wtd/slotsengine/SlotsEngineApplication.java
@@ -0,0 +1,13 @@
+package wtd.slotsengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlotsEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlotsEngineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/wtd/slotsengine/SlotsEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package wtd.slotsengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlotsEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
