41438a52d874adf85121fc035bd96007e61afc5f
WakeTrainDev <175499942+waketraindev@users.noreply.github.com>
2024-12-01 22:40:59 +0200

    Start implementing the slot logic

--- src/main/java/wtd/slotsengine/rest/controllers/ApiController.java
+++ src/main/java/wtd/slotsengine/rest/controllers/ApiController.java
@@ -12,6 +12,8 @@ import wtd.slotsengine.rest.exceptions.InvalidSubscriberException;
 import wtd.slotsengine.rest.records.ServerVersionMessage;
 import wtd.slotsengine.services.LiveEventsManager;
 import wtd.slotsengine.services.LiveSubscriber;
+import wtd.slotsengine.services.SlotManager;
+import wtd.slotsengine.slots.interfaces.SlotMachine;
 import wtd.slotsengine.utils.SlotUtils;
 
 import java.util.UUID;
@@ -21,10 +23,14 @@ public class ApiController implements InitializingBean {
     private static final ServerVersionMessage SERVER_BANNER = new ServerVersionMessage(SlotUtils.PROJECT_VERSION);
     private static final Logger log = LoggerFactory.getLogger(ApiController.class);
     private final LiveEventsManager live;
+    private final SlotManager sm;
+    private final SlotMachine machine;
 
-    public ApiController(LiveEventsManager liveEventsManager) {
+    public ApiController(LiveEventsManager liveEventsManager, SlotManager slotManager) {
         log.info("API controller is initializing");
         this.live = liveEventsManager;
+        this.sm = slotManager;
+        this.machine = slotManager.getSlotMachine();
     }
 
     @Override
@@ -35,6 +41,14 @@ public class ApiController implements InitializingBean {
     @GetMapping("/api")
     public ServerVersionMessage indexAction() {
         return SERVER_BANNER;
+    }
+
+    @GetMapping("/api/spin")
+    public String spin() {
+        StringBuilder result = new StringBuilder();
+        long winAmount = machine.spin(1);
+        result.append("You won ").append(winAmount).append(" credits.");
+        return result.toString();
     }
 
     @GetMapping("/api/{uid}")
--- src/main/java/wtd/slotsengine/services/SlotManager.java
+++ src/main/java/wtd/slotsengine/services/SlotManager.java
@@ -1,7 +1,18 @@
 package wtd.slotsengine.services;
 
 import org.springframework.stereotype.Service;
+import wtd.slotsengine.slots.interfaces.SlotMachine;
+import wtd.slotsengine.slots.machines.BasicSlotMachine;
 
 @Service
 public class SlotManager {
+    private final SlotMachine sm;
+
+    public SlotManager() {
+        sm = new BasicSlotMachine();
+    }
+
+    public SlotMachine getSlotMachine() {
+        return sm;
+    }
 }
