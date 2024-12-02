39c8b2bbd72b0dc8cae091baa8abffc35e9b1d61
WakeTrainDev <175499942+waketraindev@users.noreply.github.com>
2024-12-02 17:05:33 +0200

    Checkpoint

--- src/main/resources/static/scripts/app.js
+++
@@ -1,7 +0,0 @@
-export class MainApp {
-    constructor() {
-    }
-
-    start() {
-    }
-}
--- src/main/resources/static/scripts/main.js
+++ src/main/resources/static/scripts/main.js
@@ -1,7 +1,29 @@
-import {MainApp} from "./app.js";
+window.onload = (function () {
+    startApp();
+});
 
-(function () {
-    let app = new MainApp();
-    app.start();
-    window.app = app;
-})();
+function startApp() {
+    console.log("Starting app");
+    const spinButton = document.getElementById("spinButton");
+    const spinResult = document.getElementById("spinResult");
+
+    spinButton.addEventListener("click", () => {
+        spinResult.innerHTML = "Spinning...";
+        fetch("/api/debugspin").then(response => response.text()).then(text => {
+            spinResult.innerHTML = text;
+            console.log(text);
+        })
+    });
+
+    const liveEvents = new EventSource("/events");
+    liveEvents.onmessage = function () {
+        console.log("New message");
+        console.log(event);
+    };
+    liveEvents.onopen = function (event) {
+        console.log("Connection opened");
+    }
+    window.es = liveEvents;
+    console.log(liveEvents)
+
+}
