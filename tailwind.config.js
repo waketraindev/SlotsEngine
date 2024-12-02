084c959479e91cbdfc114cab0cd95e2244dd80e3
WakeTrainDev <175499942+waketraindev@users.noreply.github.com>
2024-12-02 23:29:28 +0200

    Switch from TailwindCSS to Bootstrap
    This commit removes all files related to TailwindCSS, including the configuration files, CSS, JavaScript, and package.json entries. It introduces Bootstrap as the new front-end framework by adding the new Bootstrap JavaScript file. This change aims to streamline styling and functionality in the application.

--- tailwind.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: ['./src/main/resources/static/index.html', './src/main/resources/static/**/*.{js,ts,jsx,tsx}'], theme: {
-        extend: {},
-    }, plugins: [],
-}
