5283633a6068c3fb07c205655240398f6041857f
WakeTrainDev <175499942+waketraindev@users.noreply.github.com>
2024-12-03 16:01:32 +0200

    Update project version to 0.0.9
    Updated the project version from 0.0.8.1 to 0.0.9 in SlotConstants.java and pom.xml. This change is necessary to reflect the latest developments and improvements in the project. Ensure that all dependencies and build processes are aligned with this new version tag.

--- src/main/java/wtd/slotsengine/utils/SlotConstants.java
+++ src/main/java/wtd/slotsengine/utils/SlotConstants.java
@@ -1,6 +1,6 @@
 package wtd.slotsengine.utils;
 
 public final class SlotConstants {
-    public static final String PROJECT_VERSION = "0.0.8.1";
+    public static final String PROJECT_VERSION = "0.0.9";
     public static final String DEMO_MACHINE = "H4sIAAAAAAAA/2NgGAW0A4zYARMmYEYHLMiAFQHYIIAdBDg4ODg5uQARLQflwwEAAA==";
 }
