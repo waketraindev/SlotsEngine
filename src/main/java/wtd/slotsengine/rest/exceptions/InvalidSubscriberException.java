605ab83890969055cb061377af23a4acf317f225
WakeTrainDev <175499942+waketraindev@users.noreply.github.com>
2024-12-03 14:17:34 +0200

    Remove InvalidSubscriberException class.
    The InvalidSubscriberException is no longer needed and has been deleted to streamline the exception handling process. This removal helps maintain cleaner code and reduces unnecessary exception classes.

--- src/main/java/wtd/slotsengine/rest/exceptions/InvalidSubscriberException.java
+++
@@ -1,7 +0,0 @@
-package wtd.slotsengine.rest.exceptions;
-
-public class InvalidSubscriberException extends Exception {
-    public InvalidSubscriberException(String message) {
-        super(message);
-    }
-}
