53e70338b6746df337fb6be33b1603b5055fadfd
WakeTrainDev <175499942+waketraindev@users.noreply.github.com>
2024-12-03 09:44:43 +0200

    Add keyboard shortcuts for button actions in main.js
    Implemented key listeners for 'a', 's', and 'd' to trigger spin, increment bet, and decrement bet actions, respectively. This enhancement improves user interaction by allowing keyboard control.

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,1 +1,17 @@
-console.log("Hello world");
+let btnSpin = document.getElementById('btnSpin');
+let btnIncBet = document.getElementById('btnIncrementBet');
+let btnDecBet = document.getElementById('btnDecrementBet');
+
+document.addEventListener('keyup', (e) => {
+    switch (e.key) {
+        case 'a':
+            btnSpin.click();
+            break;
+        case 's':
+            btnIncBet.click();
+            break;
+        case 'd':
+            btnDecBet.click();
+            break;
+    }
+});
