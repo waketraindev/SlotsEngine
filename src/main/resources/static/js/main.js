605cb5f6f71a85a3668204b7fcb7763dd6d8006a
WakeTrainDev <175499942+waketraindev@users.noreply.github.com>
2024-12-03 10:37:37 +0200

    Add balance management and dynamic betting feature
    This commit introduces deposit and withdrawal prompts for managing the user's balance in the application. It also updates the spin function to use the bet amount dynamically and modifies the button event listeners to adjust the bet amount, reflecting it in the UI. Additionally, a new `machineState` object is introduced to manage the state of balance and bet amount efficiently.

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -2,16 +2,30 @@ let appwindow = document.getElementById('appwin');
 let btnSpin = document.getElementById('btnSpin');
 let btnIncBet = document.getElementById('btnIncrementBet');
 let btnDecBet = document.getElementById('btnDecrementBet');
+
+let btnDeposit = document.getElementById('btnDeposit');
+let btnWithdraw = document.getElementById('btnWithdraw');
+
 let lblBalanceAmount = document.getElementById('lblBalanceAmount');
 let lblBetAmount = document.getElementById('lblBetAmount');
 
+let lblDisplay = document.getElementById('lblDisplay');
+
 let lastSpin;
 
+let machineState = {
+    balance: 0, betAmount: 1
+};
+
 function spin() {
-    fetch('/api/spin').then(response => response.json()).then(data => {
+    fetch('/api/spin/' + machineState.betAmount, {
+        method: 'POST'
+    }).then(response => response.json()).then(data => {
         lastSpin = data;
         lblBetAmount.innerText = data.betAmount;
         lblBalanceAmount.innerText = data.balance;
+        lblDisplay.innerText = data.result;
+        console.log(data);
     })
 }
 
@@ -29,16 +43,26 @@ document.addEventListener('keyup', (e) => {
     }
 });
 
+btnDeposit.addEventListener('click', () => {
+    let value = prompt("Enter deposit amount", 1000);
+})
+btnWithdraw.addEventListener('click', () => {
+    let value = prompt("Enter withdrawal amount", 1000);
+})
 btnSpin.addEventListener('click', () => {
-    console.log("Spin");
     spin();
 });
 btnIncBet.addEventListener('click', () => {
-    console.log("Increment");
+    machineState.betAmount += 1;
+    lblBetAmount.innerText = machineState.betAmount;
 });
 btnDecBet.addEventListener('click', () => {
-    console.log("Decrement");
+    if (machineState.betAmount > 1) {
+        machineState.betAmount -= 1;
+        lblBetAmount.innerText = machineState.betAmount;
+    }
 });
+
 
 // Display UI after loading
 window.addEventListener('load', () => {
