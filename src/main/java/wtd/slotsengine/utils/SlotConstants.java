4d35942dcbb4e8eebf83007c29c5af620a435779
WakeTrainDev <175499942+waketraindev@users.noreply.github.com>
2024-12-04 14:51:19 +0200

    Update demo machine constant in SlotConstants.java
    Replaced the existing DEMO_MACHINE constant with a new encoded string to ensure compatibility with recent changes in the slots engine. This update is crucial for maintaining the integrity of demo simulations. Make sure to verify the changes in the relevant parts of the application.

--- src/main/java/wtd/slotsengine/utils/SlotConstants.java
+++ src/main/java/wtd/slotsengine/utils/SlotConstants.java
@@ -2,5 +2,5 @@ package wtd.slotsengine.utils;
 
 public final class SlotConstants {
     public static final String PROJECT_VERSION = "0.1.0";
-    public static final String DEMO_MACHINE = "H4sIAAAAAAAA/2NgGAW0A4zYARMmYEYHLMiAFQHYIIAdBDg4ODg5uQARLQflwwEAAA==";
+    public static final String DEMO_MACHINE = "H4sIAAAAAAAA/2NgGAWDHTASBkxEAGYcgAUNsCIDNghgBwMOMODk5OQCANokw5M3AgAA";
 }
