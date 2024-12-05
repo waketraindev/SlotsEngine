b0b135a596562d2e572ca6d4cc8baad42dbdbfbd
WakeTrainDev <175499942+waketraindev@users.noreply.github.com>
2024-12-05 11:15:40 +0200

    Add VirtualReelBuilder for reel construction and operations
    Introduces a new VirtualReelBuilder class to facilitate building virtual reels with methods for adding symbols, sorting, and shuffling them. This change enables a more flexible and modular approach to constructing virtual reels in the slots engine.

--- src/main/java/wtd/slotsengine/slots/machines/abstracts/VirtualReelBuilder.java
+++ src/main/java/wtd/slotsengine/slots/machines/abstracts/VirtualReelBuilder.java
@@ -1,4 +1,37 @@
 package wtd.slotsengine.slots.machines.abstracts;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class VirtualReelBuilder {
+    private final ArrayList<Byte> newList;
+
+    public VirtualReelBuilder() {
+        this.newList = new ArrayList<>();
+    }
+
+    public static VirtualReelBuilder builder() {
+        return new VirtualReelBuilder();
+    }
+
+    public VirtualReelBuilder addSymbol(int symbol, int count) {
+        for (int i = 0; i < count; i++) {
+            newList.add((byte) symbol);
+        }
+        return this;
+    }
+
+    public VirtualReelBuilder sort() {
+        Collections.sort(newList);
+        return this;
+    }
+
+    public VirtualReelBuilder shuffle() {
+        Collections.shuffle(newList);
+        return this;
+    }
+
+    public VirtualReel build() {
+        return new VirtualReel(newList);
+    }
 }
