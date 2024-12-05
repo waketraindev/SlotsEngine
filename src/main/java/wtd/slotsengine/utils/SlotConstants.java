a06787a0ff78014cf3e41ed0813b4e930b3e6cd6
WakeTrainDev <175499942+waketraindev@users.noreply.github.com>
2024-12-05 12:58:25 +0200

    Use Spring @Value for dynamic versioning
    Replaced hardcoded version with @Value to dynamically load the application version from configuration. This change enhances maintainability and allows for easier updates by removing the need to modify the source code for version changes. Updated the ApiController to use the new version property for the server banner response.

--- src/main/java/wtd/slotsengine/utils/SlotConstants.java
+++ src/main/java/wtd/slotsengine/utils/SlotConstants.java
@@ -1,6 +1,5 @@
 package wtd.slotsengine.utils;
 
 public final class SlotConstants {
-    public static final String PROJECT_VERSION = "0.1.2";
     public static final String DEMO_MACHINE = "H4sIAAAAAAAA/+3BxwGAIAAAMRQb7j+wQ9zTJGNr9mY2R3M2V3M3T7OatxkAAAAAAAAAAH/1AbtNSOuwIwAA";
 }
