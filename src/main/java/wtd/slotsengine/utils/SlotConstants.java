e5ffea19181229a9d2ebd32143b437a9b21168e3
WakeTrainDev <175499942+waketraindev@users.noreply.github.com>
2024-12-07 10:15:42 +0200

    Use custom random instance for shuffle
    Introduced a static Random instance in SlotConstants and used it to shuffle the reel list in VirtualReel. This change ensures that the shuffling process is consistent and potentially allows for better control over the randomization used within the slots engine.

--- src/main/java/wtd/slotsengine/utils/SlotConstants.java
+++ src/main/java/wtd/slotsengine/utils/SlotConstants.java
@@ -1,6 +1,9 @@
 package wtd.slotsengine.utils;
 
+import java.util.Random;
+
 public final class SlotConstants {
     //Best RTP: 0.9799932180400136: H4sIAAAAAAAA/+3ByQEAIAjAMBQv3H9gd7DfJtGYziQzmMksZjOHqW83JEmSxDwCVlXqhQsAAA== Size: 2949
     public static final String DEMO_MACHINE = "H4sIAAAAAAAA/+3ByQEAIAjAMBQv3H9gd7DfJtGYziQzmMksZjOHqW83JEmSxDwCVlXqhQsAAA==";
+    public static final Random RANDOM = new Random();
 }
