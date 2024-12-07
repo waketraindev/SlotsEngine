d991cd083b02b2fbf5023f52416b90e9a3a90759
WakeTrainDev <175499942+waketraindev@users.noreply.github.com>
2024-12-07 10:34:28 +0200

    Update DEMO_MACHINE constant with new encoded value
    The DEMO_MACHINE constant in SlotConstants.java has been updated to reflect a new encoded string while maintaining its Best RTP value. This change could potentially impact any functionality depending on the previous string, so verification of dependent components is advisable.

--- src/main/java/wtd/slotsengine/utils/SlotConstants.java
+++ src/main/java/wtd/slotsengine/utils/SlotConstants.java
@@ -4,6 +4,8 @@ import java.util.Random;
 
 public final class SlotConstants {
     //Best RTP: 0.9799932180400136: H4sIAAAAAAAA/+3ByQEAIAjAMBQv3H9gd7DfJtGYziQzmMksZjOHqW83JEmSxDwCVlXqhQsAAA== Size: 2949
-    public static final String DEMO_MACHINE = "H4sIAAAAAAAA/+3ByQEAIAjAMBQv3H9gd7DfJtGYziQzmMksZjOHqW83JEmSxDwCVlXqhQsAAA==";
+    //public static final String DEMO_MACHINE = "H4sIAAAAAAAA/+3ByQEAIAjAMBQv3H9gd7DfJtGYziQzmMksZjOHqW83JEmSxDwCVlXqhQsAAA==";
+    //Best RTP: 0.9799932180400136: H4sIAAAAAAAA/+3ByQEAIAjAMBQv3H9gd7DfJhGSJEliGtOZZAYzmcVs5jD17T6rGHcKhQsAAA== Size: 2949
+    public static final String DEMO_MACHINE = "H4sIAAAAAAAA/+3ByQEAIAjAMBQv3H9gd7DfJhGSJEliGtOZZAYzmcVs5jD17T6rGHcKhQsAAA==";
     public static final Random RANDOM = new Random();
 }
