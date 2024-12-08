1ae5bc954eeaf0c2f660914e8205efe2626e445f
WakeTrainDev <175499942+waketraindev@users.noreply.github.com>
2024-12-08 16:49:10 +0200

    Update DEMO_MACHINE constant for improved RTP
    The DEMO_MACHINE constant has been updated to a new value with a better RTP of 0.98750000, which ensures better performance. The new configuration also reflects a larger size of 6960, providing enhanced machine logic.

--- src/main/java/wtd/slotsengine/utils/SlotConstants.java
+++ src/main/java/wtd/slotsengine/utils/SlotConstants.java
@@ -3,7 +3,7 @@ package wtd.slotsengine.utils;
 import java.util.Random;
 
 public final class SlotConstants {
-    //Best RTP: 0.9799943646097492: H4sIAAAAAAAA/+3BtwEAIAzAsNDL/wcz8AFeGCxFSJIkSfpLYjJTmMo0piMDmch6t68Dw5sLyN0NAAA= Size: 3549
-    public static final String DEMO_MACHINE = "H4sIAAAAAAAA/+3BtwEAIAzAsNDL/wcz8AFeGCxFSJIkSfpLYjJTmMo0piMDmch6t68Dw5sLyN0NAAA=";
+    // Best RTP:	0.98750000:	/	Size:	6960	/	H4sIAAAAAAAA/+3BwQ2AIBAAMFAUYf+B+TmBkculbSkAAAAAwE8qiRxscfK1lscVzh1ID+MJY7zmAvom1HkwGwAA
+    public static final String DEMO_MACHINE = "H4sIAAAAAAAA/+3BwQ2AIBAAMFAUYf+B+TmBkculbSkAAAAAwE8qiRxscfK1lscVzh1ID+MJY7zmAvom1HkwGwAA";
     public static final Random RANDOM = new Random();
 }
