4456aaf6850cac96b227d92bb101b65de03a762d
WakeTrainDev <175499942+waketraindev@users.noreply.github.com>
2024-12-08 18:33:37 +0200

    Update DEMO_MACHINE constant in SlotConstants.java
    Changed the DEMO_MACHINE string to reflect the new machine configuration with a slightly adjusted RTP and size. This update helps keep the data consistent with the latest slot machine settings and optimizes the simulation parameters accordingly.

--- src/main/java/wtd/slotsengine/utils/SlotConstants.java
+++ src/main/java/wtd/slotsengine/utils/SlotConstants.java
@@ -3,7 +3,7 @@ package wtd.slotsengine.utils;
 import java.util.Random;
 
 public final class SlotConstants {
-    // Best RTP:	0.98750000:	/	Size:	6960	/	H4sIAAAAAAAA/+3BwQ2AIBAAMFAUYf+B+TmBkculbSkAAAAAwE8qiRxscfK1lscVzh1ID+MJY7zmAvom1HkwGwAA
-    public static final String DEMO_MACHINE = "H4sIAAAAAAAA/+3BwQ2AIBAAMFAUYf+B+TmBkculbSkAAAAAwE8qiRxscfK1lscVzh1ID+MJY7zmAvom1HkwGwAA";
+    //Best RTP:	0.98000000:	/	Size:	7350	/	H4sIAAAAAAAA/+3BOw6AIBQAMBSUz/0P7OzgYDDxhbRNCQAAAADgBxsEsrOWTARlSUcA5wfqhPZSfzDuLvMf11u2HAAA
+    public static final String DEMO_MACHINE = "H4sIAAAAAAAA/+3BOw6AIBQAMBSUz/0P7OzgYDDxhbRNCQAAAADgBxsEsrOWTARlSUcA5wfqhPZSfzDuLvMf11u2HAAA";
     public static final Random RANDOM = new Random();
 }
