e2bdd4fe157bd53745548c7895c2a06443d48176
WakeTrainDev <175499942+waketraindev@users.noreply.github.com>
2024-12-10 13:42:12 +0200

    Refactor SimpleReelGenerator for efficiency and clarity
    Replaced ITemp with BufferedSymbols to streamline symbol addition. Utilized a single Random instance for improved performance. Enhanced code readability and maintainability by making method parameters final and adjusting class field modifiers.

--- src/main/java/wtd/slotsengine/utils/SimpleReelGenerator.java
+++ src/main/java/wtd/slotsengine/utils/SimpleReelGenerator.java
@@ -2,26 +2,27 @@ package wtd.slotsengine.utils;
 
 import wtd.slotsengine.slots.machines.reels.VirtualReel;
 
+import java.util.Random;
 import java.util.concurrent.*;
 
 public class SimpleReelGenerator {
-    private final int BOUND = 256;
-    private final double maxRtp;
-    private final int historySize;
-    private final double[] history;
+    private final Random random = new Random();
     private final ExecutorService exec = Executors.newWorkStealingPool();
-    private final long[] payoutTable = new long[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 100};
+    private final double maxRtp;
+    private final double[] history;
+    private final int historySize;
+    private final long[] payoutTable = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 100};
     private double bestRtp = 0.0;
     private VirtualReel bestReel;
 
-    public SimpleReelGenerator(double maxRtp) {
+    public SimpleReelGenerator(final double maxRtp) {
         this.maxRtp = maxRtp;
         this.historySize = 1024;
         this.history = new double[historySize];
     }
 
-    public static void main(String[] args) {
-        double maxRtp = 0.98;
+    public static void main(final String[] args) {
+        final double maxRtp = 0.98;
         SimpleReelGenerator gen = new SimpleReelGenerator(maxRtp);
         try {
             gen.run(new TimeStopCondition(60, TimeUnit.MINUTES));
@@ -30,11 +31,10 @@ public class SimpleReelGenerator {
         }
     }
 
-    public void run(GenStopCondition stopCondition) throws ExecutionException, InterruptedException {
+    public void run(final GenStopCondition stopCondition) throws ExecutionException, InterruptedException {
         int runCount = 0;
+        @SuppressWarnings("unchecked") Future<PResult>[] ret = new Future[historySize * 10];
         while (stopCondition.apply(runCount)) {
-            @SuppressWarnings("unchecked")
-            Future<PResult>[] ret = new Future[historySize];
             for (int i = 0; i < ret.length; i++) {
                 ret[i] = exec.submit(this::generateReel);
             }
@@ -56,24 +56,24 @@ public class SimpleReelGenerator {
     }
 
     private PResult generateReel() {
-        final int rand10 = boundRand(0), rand9 = boundRand(rand10), rand8 = boundRand(rand9), rand7 =
-                boundRand(rand8), rand6 = boundRand(rand7), rand5 = boundRand(rand6), rand4 = boundRand(rand5), rand3 =
-                boundRand(rand4), rand2 = boundRand(rand3), rand1 = boundRand(rand2);
+        final int rand10 = boundRand(0), rand9 = boundRand(rand10), rand8 = boundRand(rand9), rand7 = boundRand(rand8),
+                rand6 = boundRand(rand7), rand5 = boundRand(rand6), rand4 = boundRand(rand5), rand3 = boundRand(rand4),
+                rand2 = boundRand(rand3), rand1 = boundRand(rand2);
         final byte[] reel = new byte[rand1 + rand2 + rand3 + rand4 + rand5 + rand6 + rand7 + rand8 + rand9 + rand10];
-        ITemp fl = new ITemp();
-        fl.addSymbol(reel, (byte) 1, rand1);
-        fl.addSymbol(reel, (byte) 2, rand2);
-        fl.addSymbol(reel, (byte) 3, rand3);
-        fl.addSymbol(reel, (byte) 4, rand4);
-        fl.addSymbol(reel, (byte) 5, rand5);
-        fl.addSymbol(reel, (byte) 6, rand6);
-        fl.addSymbol(reel, (byte) 7, rand7);
-        fl.addSymbol(reel, (byte) 8, rand8);
-        fl.addSymbol(reel, (byte) 9, rand9);
-        fl.addSymbol(reel, (byte) 10, rand10);
+        BufferedSymbols fl = new BufferedSymbols();
+        fl.add(reel, (byte) 1, rand1);
+        fl.add(reel, (byte) 2, rand2);
+        fl.add(reel, (byte) 3, rand3);
+        fl.add(reel, (byte) 4, rand4);
+        fl.add(reel, (byte) 5, rand5);
+        fl.add(reel, (byte) 6, rand6);
+        fl.add(reel, (byte) 7, rand7);
+        fl.add(reel, (byte) 8, rand8);
+        fl.add(reel, (byte) 9, rand9);
+        fl.add(reel, (byte) 10, rand10);
         double cost = 0L, winAmount = 0L;
-        for (int j : reel) {
-            winAmount += calculatePayout(j);
+        for (final int symbol : reel) {
+            winAmount += calculatePayout(symbol);
             cost++;
         }
         double rtp = winAmount / cost;
@@ -88,10 +88,8 @@ public class SimpleReelGenerator {
         return new PResult(rtp, finalReel);
     }
 
-
-    private int boundRand(int lo) {
-        final ThreadLocalRandom random = ThreadLocalRandom.current();
-        return random.nextInt(lo + 1, BOUND + lo);
+    private int boundRand(final int lo) {
+        return random.nextInt(lo + 1, lo + 256);
     }
 
     private long calculatePayout(final int symbol) {
@@ -101,22 +99,22 @@ public class SimpleReelGenerator {
     private record PResult(double rtp, byte[] rb) {
     }
 
-    private static class ITemp {
-        int flipIndex = 0;
+    private static class BufferedSymbols {
+        private int index = 0;
 
-        private void addSymbol(byte[] reel, byte sym, int count) {
-            final int end = flipIndex + count, batch = 8;
-            for (; flipIndex + batch < end; flipIndex += batch) {
-                reel[flipIndex] = sym;
-                reel[flipIndex + 1] = sym;
-                reel[flipIndex + 2] = sym;
-                reel[flipIndex + 3] = sym;
-                reel[flipIndex + 4] = sym;
-                reel[flipIndex + 5] = sym;
-                reel[flipIndex + 6] = sym;
-                reel[flipIndex + 7] = sym;
+        private void add(final byte[] reel, final byte sym, final int count) {
+            final int end = index + count, batch = 8;
+            for (; index + batch < end; index += batch) {
+                reel[index] = sym;
+                reel[index + 1] = sym;
+                reel[index + 2] = sym;
+                reel[index + 3] = sym;
+                reel[index + 4] = sym;
+                reel[index + 5] = sym;
+                reel[index + 6] = sym;
+                reel[index + 7] = sym;
             }
-            for (; flipIndex < end; flipIndex++) reel[flipIndex] = sym;
+            for (; index < end; index++) reel[index] = sym;
         }
     }
 }
